55b825024daf26da567401c6e2ff40d9466c6df2
tamicristina <tami.cristina06@gmail.com>
2021-06-21 19:01:00 -0300

    Dados adicionados ao local storage

--- main.js
+++ main.js
@@ -9,17 +9,26 @@ botaoLimpar.addEventListener("click", limparItem);
 function adicionarItem() {
   let adicionar = "<li class=itens>" + item.value;
 
-  if (lista.innerText.includes(item.value)) {
+  if (lista.innerText.includes(item.value.toLowerCase())) {
     window.alert("O item informado já existe na lista");
   } else {
-    lista.innerHTML += adicionar;
+    lista.innerHTML += adicionar.toLowerCase();
   }
 
   item.value = "";
   item.focus();
-  return adicionar;
 }
 
 function limparItem() {
   lista.innerHTML = "";
+  return;
 }
+document.querySelector("ul").value = localStorage.item;
+const salvarDados = function () {
+  let itensLista = lista.innerText;
+
+  localStorage.setItem("item", itensLista);
+};
+
+document.onchange = salvarDados;
+localStorage.clear();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Lista de Compras
+
+## Exercícios proposto no Curso Estartando Devs, com o objetivo de criar um CRUD e armazenar os dados no LocalStorage
+
+<img alt="NextLevelWeek" src="./assets/img/lista_de_compras.png" />
